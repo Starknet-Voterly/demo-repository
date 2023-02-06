15dada71f521a838aada72a871de60d9cf909e35
jonahb27 <>
2023-02-06 18:03:37 +0200

    Added warp testing

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nethermindeth/hardhat-warp"); //comment out to use normal harhat (comment in to use starknet)
+// TO RUN THE STARKNET DEVNET
+//source venv/bin/activate
+//starknet-devnet
+
+// TO INSTALL THE STARKNET DEVNET
+// env "CFLAGS=-I/usr/local/include -L/usr/local/lib" pip install starknet-devnet
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+  integratedDevnet: {
+    url: `http://127.0.0.1:5050`,
+
+    venv: "./venv",
+    args: ["--seed", "0", "--timeout", "10000"],
+    stdout: `stdout.log`, // <- logs redirected to log file
+    stderr: "STDERR"  // <- logs stderr to the terminal
+  },
+},
 };
